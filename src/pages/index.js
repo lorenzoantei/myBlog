9c16b7b46779395fa98461ac93c96a198004f74e
Lorenzo Antei <lorenzoantei@protonmail.com>
2022-03-06 18:29:33 +0100

    cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,11 @@ import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="About">
+    <Layout pageTitle="home">
       
       <div className="text-grey-500 font-poppins">
         <p>I'm a digital media maker.</p>
-        <p>Since 2016 I have been making photostories, recording videos, making frontends, trying to do stuff with JS. I study at the Academy of Fine Arts in Carrara and work around Italy.</p>
+        <p>Since 2016 I have been making photostories, recording videos and trying to do stuff with JS. I study at the Academy of Fine Arts in Carrara and work around Italy.</p>
       </div>
 
       <StaticImage className="my-8" src="../images/sb_omen_v20034.jpg" />
