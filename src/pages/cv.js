5a4c46c2da377a0deab0ed440e6db3778a6666a7
Lorenzo Antei <58749463+lorenzoantei@users.noreply.github.com>
2022-03-16 07:59:08 +0100

    Update cv.js
    fix links and hover

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -18,17 +18,17 @@ const IndexPage = ({ data }) => {
   
           <div class="py-6">
             <h3 className="font-poppins text-xl text-gray-500 py-3">Selezione Milan Machinima Festival '22</h3>
-          <p>Il mio machinima "Clouds" è stato selezionato per lo screening del Milan Machinima Festival. <a class="hover:underline" href="https://milanmachinimafestival.org/">Link all'intervista</a></p>
+          <p>Il mio machinima "Clouds" è stato selezionato per lo screening del Milan Machinima Festival. <a class="text-gray-400 hover:text-black hover:ease-in-out duration-1000" href="https://milanmachinimafestival.org/">Link all'intervista</a></p>
           </div>
           
           <div class="py-6">
             <h3 className="font-poppins text-xl text-gray-500 py-3">matrimonio.com – scansione 3D location (da luglio ‘21 a ottobre ‘21)</h3>
-            <p>Ho realizzato una serie di tour virtuali (scansioni 3D e foto sferiche a 360º) delle location matrimoniali con Matterport Pro2 per matrimonio.com </p>
+            <p>Ho realizzato una serie di tour virtuali (scansioni 3D e foto sferiche a 360º) delle location matrimoniali con Matterport Pro2 per <a class="text-gray-400 hover:text-black hover:ease-in-out duration-1000" href="https://www.matrimonio.com">matrimonio.com</a></p>
           </div>
           
           <div class="py-6">
             <h3 className="font-poppins text-xl text-gray-500 py-3">Carnet Erotico - Francesca Zaccaria (luglio ‘21)</h3>
-          <p>Ho prodotto del materiale fotografico ed audiovisivo per la Danzatrice Francesca Zaccaria (ALDES | SPAM!). <a class="hover:underline" href="https://www.aldesweb.org/produzioni/carnet-erotico/">Link al progetto</a></p>
+          <p>Ho prodotto del materiale fotografico ed audiovisivo per la Danzatrice Francesca Zaccaria (ALDES | SPAM!). <a class="text-gray-400 hover:text-black hover:ease-in-out duration-1000" href="https://www.aldesweb.org/produzioni/carnet-erotico/">Link al progetto</a></p>
           </div>
   
           <div class="py-6">
@@ -37,14 +37,14 @@ const IndexPage = ({ data }) => {
           </div>
   
           <div class="py-6">
-            <h3 className="font-poppins text-xl text-gray-500 py-3">Raquel Torralba – Fotografia matrimoniale (da maggio ‘21 a settembre '21')</h3>
-            <p>Studio fotografico specializzato nella realizzazione di servizi foto e video per matrimoni in stile reportage. Produzione di materiale fotografico e audiovisivo. Raquel Torralba Fotografia (Via Cairoli 6/8, 16124 Genova) - raqueltorralba.com</p>
+            <h3 className="font-poppins text-xl text-gray-500 py-3">Raquel Torralba – <a class="text-gray-400 hover:text-black hover:ease-in-out duration-1000" href="https://www.flickr.com/photos/193622395@N05/sets/72157719705710180/with/51632770803/">Fotografia matrimoniale</a> (da maggio ‘21 a settembre '21')</h3>
+            <p>Studio fotografico specializzato nella realizzazione di servizi foto e video per matrimoni in stile reportage. Produzione di materiale fotografico e audiovisivo. Raquel Torralba Fotografia (Via Cairoli 6/8, 16124 Genova) - <a class="hover:underline" href="https://www.raqueltorralba.com/">raqueltorralba.com</a></p>
           </div>
   
           <div class="py-6">
             <h3 className="font-poppins text-xl text-gray-500 py-3">Giovanni Antonio Cybei e il suo tempo – web design (aprile ‘21)</h3>
             <p>La mostra “Giovanni Antonio Cybei e il suo tempo. Insigne statuario per le corti europee e Primario Direttore dell’Accademia di Belle Arti di Carrara” celebra il primo direttore dell’Accademia, lo scultore carrarese Giovanni Antonio Cybei (1706-1784), che operò per le più illustri case regnanti d’Italia e d’Europa. È promossa e realizzata dall’Accademia di Belle Arti di Carrara ed ideata e curata dal Prof. Gerardo de Simone e da Luciano Massari, Direttore dell’Accademia di Belle Arti di Carrara. Per la mostra, ho ideato e realizzato il design del sito web dell’evento.</p>
-            <a class="hover:underline" href="https://www.accademiacarrara.it/img/mcybei/index.html">Link al sito della mostra</a>
+            <a class="text-gray-400 hover:text-black hover:ease-in-out duration-1000" href="https://www.accademiacarrara.it/img/mcybei/index.html">Link al sito della mostra</a>
           </div>
           
           <div class="py-6">
@@ -59,9 +59,9 @@ const IndexPage = ({ data }) => {
           </div>
           
           <div class="py-6">
-            <h3 className="font-poppins text-xl text-gray-500 py-3">Foto Bouquet - fotografia matrimoniale (da settembre '20 a settembre '21)</h3>
+            <h3 className="font-poppins text-xl text-gray-500 py-3">Foto Bouquet - <a class="text-gray-400 hover:text-black hover:ease-in-out duration-1000" href="https://www.flickr.com/photos/193622395@N05/sets/72157719705710180/with/51632770803/">Fotografia matrimoniale</a> (da settembre '20 a settembre '21)</h3>
             <p>Fotobouquet è uno studio fotografico specializzato nella realizzazione di servizi foto e video per matrimoni in stile reportage. Produzione di materiale fotografico e audiovisivo.
-              Studio Fotografico Foto Bouquet (Via Montebello, 7 - 40124 Bologna) - <a class="hover:underline" href="https://www.foto-bouquet.com/">foto-bouquet.com</a></p>
+              Studio Fotografico Foto Bouquet (Via Montebello, 7 - 40124 Bologna) - <a class="text-gray-400 hover:text-black hover:ease-in-out duration-1000" href="https://www.foto-bouquet.com/">foto-bouquet.com</a></p>
           </div>
   
           <div class="py-6">
@@ -72,31 +72,31 @@ const IndexPage = ({ data }) => {
           <div class="py-6">
             <h3 className="font-poppins text-xl text-gray-500 py-3">Virtuitaly (da marzo ‘19 a giugno ‘19)</h3>
             <p>VirtuItaly è una startup company, nata come spinoff di Centrica con lo scopo di valorizzare il patrimonio culturale e creativo italiano e creare mostre virtuali interattive e immersive: eventi culturali di grande impatto emotivo e valore educativo e di intrattenimento. Per Virtuitaly ho realizzato uno spot ed altri audiovisivi.
-              Virtuitaly S.r.l. (Piazza della Madonna della Neve, 5 – 50122 Firenze) – <a class="hover:underline"href="https://www.virtuitaly.com/">virtuitaly.com</a> </p>
+              Virtuitaly S.r.l. (Piazza della Madonna della Neve, 5 – 50122 Firenze) – <a class="text-gray-400 hover:text-black hover:ease-in-out duration-1000" href="https://www.virtuitaly.com/">virtuitaly.com</a> </p>
           </div>
   
           <div class="py-6">
             <h3 className="font-poppins text-xl text-gray-500 py-3">Centrica (da marzo ‘19 a giugno ‘19)</h3>
             <p>Centrica S.r.l. è specializzata nella progettazione, nello sviluppo e nella commercializzazione di soluzioni digitali nel campo di: beni culturali, turismo e forme di espressione della creatività. Per Centrica ho prodotto diversi audiovisivi e montaggi fotografici. Attualmente in rapporto per prestazioni occasionali.
-              Centrica S.r.l. (Piazza della Madonna della Neve, 5 – 50122 Firenze) –  <a class="hover:underline"href="https://www.centrica.it/">centrica.it</a> </p>
+              Centrica S.r.l. (Piazza della Madonna della Neve, 5 – 50122 Firenze) –  <a class="text-gray-400 hover:text-black hover:ease-in-out duration-1000" href="https://www.centrica.it/">centrica.it</a> </p>
           </div>
   
           <div class="py-6">
             <h3 className="font-poppins text-xl text-gray-500 py-3">Nessiah Festival (da novembre ‘18)</h3>
-            <p>Il Festival Nessiah è un festival nato per esplorare e raccontare la cultura ebraica in tutte le sue sfaccettature, mostrando la ricchezza della sua storia millenaria e la varietà delle tradizioni di luoghi diversi. Per il festival ho realizzato i reportage degli eventi, le riprese video e ho realizzato lo spot delle edizioni 2018 e 2019 e altri filmati promozionali. festivalnessiah.it 
-              <a class="hover:underline"href="https://www.festivalnessiah.it/">festivalnessiah.it</a></p>
+            <p>Il Festival Nessiah è un festival nato per esplorare e raccontare la cultura ebraica in tutte le sue sfaccettature, mostrando la ricchezza della sua storia millenaria e la varietà delle tradizioni di luoghi diversi. Per il festival ho realizzato i reportage degli eventi, le riprese video e ho realizzato lo spot delle edizioni 2018 e 2019 e altri filmati promozionali. <a class="hover:underline"href="https://www.festivalnessiah.it/">festivalnessiah.it</a>
+            </p>
           </div>
   
           <div class="py-6">
             <h3 className="font-poppins text-xl text-gray-500 py-3">Più o meno Pop (da marzo ‘18)</h3>
-            <p>Più o Meno POP è una Webzine che si occupa di eventi musicali. Per Più o Meno POP realizzo photostories di concerti e spettacoli, aiuto nella cura della pagina facebook della redazione e revisione degli articoli.
+            <p>Più o Meno POP è una Webzine che si occupa di eventi musicali. Per Più o Meno POP realizzo photostories di concerti e spettacoli, aiuto nella cura della pagina facebook della redazione e revisione degli articoli. 
             <a class="hover:underline"href="http://www.piuomenopop.it/?s=lorenzo+antei">QUI</a> i miei reportage.</p>
           </div>
   
           <div class="py-6">
             <h3 className="font-poppins text-xl text-gray-500 py-3">Radioeco.it (da settembre ‘17 a settembre ‘19)</h3>
             <p>radioeco.it è la radio degli studenti dell’Università di Pisa. Per Radioeco ho realizzato diversi reportage fotografici dei concerti, spettacoli ed eventi di varia natura, videointerviste e materiale promozionale multimediale; ho svolto anche attività di regia e di supporto tecnico ai programmi radiofonici.
-              Radioeco Studio (Via Curtatone e Montanara, 4 – 56126 Pisa) – <a class="hover:underline"href="http://www.radio-eco.it">radio-eco.it</a></p>
+              Radioeco Studio (Via Curtatone e Montanara, 4 – 56126 Pisa) – <a class="text-gray-400 hover:text-black hover:ease-in-out duration-1000" href="http://www.radio-eco.it">radio-eco.it</a></p>
           </div>
         
         </div>
@@ -106,17 +106,17 @@ const IndexPage = ({ data }) => {
   
           <div class="py-6">
             <h3 className="font-poppins text-xl text-gray-500 py-3">Nuove tecnologie dell’arte (da settembre ‘19)</h3>
-            <p>Accademia di Belle Arti di Carrara (Via Roma, 1 - 54033 Carrara) - <a class="hover:underline" href="https://www.accademiacarrara.it">accademiacarrara.it</a>.</p>
+            <p>Accademia di Belle Arti di Carrara (Via Roma, 1 - 54033 Carrara) - <a class="text-gray-400 hover:text-black hover:ease-in-out duration-1000" href="https://www.accademiacarrara.it">accademiacarrara.it</a>.</p>
           </div>
           
           <div class="py-6">
             <h3 className="font-poppins text-xl text-gray-500 py-3">Progetto Museolab (da novembre ‘18 a giugno ‘19)</h3>
-          <p>“Tecnico della realizzazione di progetti fotografici, audiovisuali, grafico editoriali e creazione di racconti per immagini”. TD Group Italia (Via Fiume Bianco, 56 - 00144 Roma) - <a class="hover:underline" href="https://www.tdgroupitalia.it/">tdgroupitalia.it</a></p>
+          <p>“Tecnico della realizzazione di progetti fotografici, audiovisuali, grafico editoriali e creazione di racconti per immagini”. TD Group Italia (Via Fiume Bianco, 56 - 00144 Roma) - <a class="text-gray-400 hover:text-black hover:ease-in-out duration-1000" href="https://www.tdgroupitalia.it/">tdgroupitalia.it</a></p>
           </div>
   
           <div class="py-6">
             <h3 className="font-poppins text-xl text-gray-500 py-3">Economia e commercio (da settembre '14 a giugno '18</h3>
-          <p>Ho conseguito 19 esami.</p>
+          <p>Ho superato 19 esami. Università degli studi di Pisa</p>
           </div>
   
         </div>
